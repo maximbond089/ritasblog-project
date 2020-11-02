43a183bedc7fecbc1dd2fdc984d2d6d2c0977c44
Maxim Bondarenko <maximbond089@gmail.com>
2020-11-02 21:41:36 +0600

    Add post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	image = models.ImageField(upload_to ='evenr_images/')
+	title = models.CharField(max_length=300)
+	date = models.DateTimeField()
+	text = models.TextField()
--- ritasblog/settings.py
+++ ritasblog/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'events.apps.EventsConfig',
-    ''
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
